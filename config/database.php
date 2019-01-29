c1a374b5cae466f4005012f1a5fadf4afa6b00c3
yassine-bbs <yassine.baba-slimane@eemi.com>
2019-01-29 11:46:19 +0100

    :whale:

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
+    | to use as your default connection for all database work.  Of course
     | you may use many connections at once using the Database library.
     |
     */
